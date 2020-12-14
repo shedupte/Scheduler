97d258091791f0237f78c4ed12f87f6823a84fdc
shedupte <shedupte@gmail.com>
2020-12-13 23:45:51 -0500

    issues regarding logo.svg

--- src/index.js
+++ src/index.js
@@ -3,6 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+//let store = createStore(reducer)
+
+//STORE -->   GLOBALIZED STATE
+
+//ACTON --> INCREMENT
+const increment= () => {
+  return {
+    name: 'INCREMENT'
+
+  }
+}
+const decrement= () => {
+  return {
+    name: 'DECREMENT'
+    
+  }
+}
+
+//REDUCER -->
+const counter = (state = 0, action) => {
+    switch(action.type){
+      case 'INCREMENT':
+        return state + 1
+      case 'DECREMENT':
+        return state -1;
+    }
+};
+
+let store = createStore(counter);
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH -->
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
